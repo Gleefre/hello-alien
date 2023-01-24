31818c3ee190528699bd24e1b2829f6ecca5c1a9
Gleefre <varedif.a.s@gmail.com>
2023-01-24 23:11:42 +0100

    Don't try to ship core as fake .so, use assets + copy to data directory instead.

--- app/jni/core/Android.mk
+++
@@ -1,8 +0,0 @@
-LOCAL_PATH := $(call my-dir)
-
-include $(CLEAR_VARS)
-
-LOCAL_MODULE := core
-LOCAL_SRC_FILES := libcore.so
-
-include $(PREBUILT_SHARED_LIBRARY)
--- app/src/main/java/hi/to/alien/HelloAlien.java
+++ app/src/main/java/hi/to/alien/HelloAlien.java
@@ -5,29 +5,67 @@ import android.widget.TextView;
 import android.os.Bundle;
 import android.util.Log;
 
+import android.content.res.AssetManager;
+import android.system.Os;
+import android.system.ErrnoException;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
 public class HelloAlien extends Activity {
   @Override
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     TextView text = new TextView(this);
-    Log.v("ALIEN", "Created text view :D ");
+    Log.v("ALIEN", "Created text view :D");
+    setupCore("alien.core");
     text.setText(getAlien());
     setContentView(text);
   }
-  public static void setupCore() {
-    // String libsPath = mContext.getApplicationInfo().nativeLibraryDir;
-    String libsPath = "/data/data/hi.to.alien/lib/";
-    String coreName = libsPath + "libcore.so";
-    setCorePath(coreName);
-    Log.v("ALIEN", "core path is set to " + coreName + " :) ");
+  public void setupCore(String coreName) {
+    String corePath = this.getFilesDir() + "";
+    String coreFullName = corePath + "/" + coreName;
+    File core = new File(corePath, coreName);
+    if (core.exists()) {
+      Log.v("ALIEN", "Core file is here: " +  coreFullName);
+    } else {
+      Log.v("ALIEN", "Core is lost! 0.0");
+      copyAsset(coreName, corePath);
+    }
+    setCorePath(coreFullName);
+    Log.v("ALIEN", "Core path is set to " + coreFullName + ", success!");
+  }
+  public void copyAsset(String assetName, String destination) {
+    try {
+      File core = new File(destination, assetName);
+      if (core.exists()) {
+        Log.v("ALIEN", "While copying asset: file is already here, abort");
+        return;
+      }
+      AssetManager assetManager = getAssets();
+      InputStream core_in = assetManager.open(assetName);
+      OutputStream core_out = new FileOutputStream(core);
+      byte[] buffer = new byte[1024];
+      int read;
+      Log.v("ALIEN", "Start copying asset [" + assetName + "] to [" + destination + "]");
+      while((read = core_in.read(buffer)) != -1){
+        core_out.write(buffer, 0, read);
+      }
+      core_in.close();
+      core_out.flush();
+      core_out.close();
+      Log.v("ALIEN", "Done!");
+    } catch(IOException e) {
+      Log.e("ALIEN", "Failed to copy asset 0.0", e);
+    }
   }
   public native String getAlien();
   public static native void setCorePath(String name);
   static {
     System.loadLibrary("sbcl");
-    Log.v("ALIEN", "sbcl loaded");
     System.loadLibrary("hello-alien");
-    Log.v("ALIEN", "hello-alien loaded");
-    setupCore();
   }
 }
