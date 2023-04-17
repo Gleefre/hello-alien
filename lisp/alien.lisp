f4a564948063f4edb473cf3497f0331bedef952c
Gleefre <varedif.a.s@gmail.com>
2023-04-17 21:42:13 +0200

    Fix issue with SBCL, make example slightly more interesting

--- lisp/alien.lisp
+++ lisp/alien.lisp
@@ -1,4 +1,14 @@
+(defparameter *clicks* 0)
+
 (define-alien-callable hello sb-alien:c-string ()
-  "Hello alien world :/")
+  (format nil "Hello alien world! [clicks: ~a]"
+          (incf *clicks*)))
+
+;; SBCL will search for HELLO function in main program by default
+;; Main program is Java program, so we need to load
+;; the libhello-alien.so library at the startup.
+(push (lambda ()
+        (sb-alien:load-shared-object "libhello-alien.so"))
+      *init-hooks*)
 
 (save-lisp-and-die "alien.core" :callable-exports '(hello))
