65221e8ee763fef855eb987a5d6266964e697903
Gleefre <varedif.a.s@gmail.com>
2023-04-21 15:11:21 +0200

    Err, C don't like hyphens

--- c/hello-alien.c
+++ c/hello-alien.c
@@ -18,8 +18,8 @@ Java_hi_to_alien_HelloActivity_initLisp(JNIEnv *env, jobject thiz, jstring path)
 
 JNIEXPORT jstring JNICALL
 Java_hi_to_alien_HelloActivity_getAlien(JNIEnv *env, jobject thiz) {
-  const char* hello-string = strdup(hello());
-  jstring java-hello-string (*env)->NewStringUTF(env, hello-string);
-  free(hello-string);
-  return java-hello-string;
+  const char* hello_string = strdup(hello());
+  jstring java_hello_string (*env)->NewStringUTF(env, hello_string);
+  free(hello_string);
+  return java_hello_string;
 }
