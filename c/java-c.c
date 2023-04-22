6b1c4c68b71486c3aa022a6d5e2cf04c24684ffe
Gleefre <varedif.a.s@gmail.com>
2023-04-22 11:17:13 +0200

    Add second wrapper

---
+++ c/java-c.c
@@ -0,0 +1,26 @@
+#include <jni.h>
+#include <string.h>
+#include <stdlib.h>
+
+extern int initialize_lisp(int argc, char **argv);
+__attribute__((visibility("default"))) char* (*hello)();
+
+int init(char* core) {
+  char *init_args[] = {"", "--core", core, "--noinform", "--disable-ldb"};
+  if (initialize_lisp(5, init_args) != 0) return -1;
+  return 0;
+}
+
+JNIEXPORT void JNICALL
+Java_hi_to_alien_HelloActivity_initLisp(JNIEnv *env, jobject thiz, jstring path) {
+  char* core_filename = strdup((*env)->GetStringUTFChars(env, path, NULL));
+  init(core_filename);
+}
+
+JNIEXPORT jstring JNICALL
+Java_hi_to_alien_HelloActivity_getAlien(JNIEnv *env, jobject thiz) {
+  char* hello_string = strdup(hello());
+  jstring java_hello_string = (*env)->NewStringUTF(env, hello_string);
+  free(hello_string);
+  return java_hello_string;
+}
