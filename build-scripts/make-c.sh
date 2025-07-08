60721335db5c80bfffa1b6038a113987cd416459
Gleefre <varedif.a.s@gmail.com>
2025-07-08 14:39:52 +0200

    Add support for 32-bit ABI in make-c.sh

--- build-scripts/make-c.sh
+++ build-scripts/make-c.sh
@@ -16,8 +16,10 @@ elif [ -z "$ABI" ]; then
     uname_arch=`adb shell uname -m`
 
     case $uname_arch in
-        x86_64) ABI=x86_64 ;;
-        aarch64) ABI=arm64-v8a ;;
+        *86 | i86pc) ABI=x86 ;;
+        *x86_64 | amd64) ABI=x86_64 ;;
+        aarch64 | arm64) ABI=arm64-v8a ;;
+        *arm*) ABI=armeabi-v7a ;;
         *) echo "Architecture $uname_arch is not supported."
            exit 1 ;;
     esac
@@ -25,8 +27,10 @@ fi
 echo "Determined target $ABI."
 
 case $ABI in
+    x86) TARGET_TAG=i686-linux-android ;;
     x86_64) TARGET_TAG=x86_64-linux-android ;;
     arm64-v8a) TARGET_TAG=aarch64-linux-android ;;
+    armeabi-v7a) TARGET_TAG=armv7a-linux-androideabi ;;
     *) echo "ABI $ABI is not supported."
        exit 1 ;;
 esac
